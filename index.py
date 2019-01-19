4ac3a6a0772073d478ac731b9e96bca82d0d382d
Andrew Price <agprice@umail.ucsb.edu>
2019-01-18 23:08:01 -0800

    AP Getting form submission running, and re-organizing around a more modular project structure.

--- index.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='templates', static_folder='static')
-
-@app.route("/")
-def hello():
-    return render_template('home.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
