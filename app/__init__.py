3e4ccb50b1bed0d9d4737aad96c67f643713fca8
Ryan Gormley <rgormley@umail.ucsb.edu>
2019-01-31 14:57:09 -0800

    RG

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config.from_object(Config)
-app.config.from_object(os.environ['APP_SETTINGS'])
+#app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 from models import Book
