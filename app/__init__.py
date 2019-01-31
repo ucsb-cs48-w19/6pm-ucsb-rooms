f56056d56cfe03be19e1522f7cc4b948537d3ea4
Ryan Gormley <rgormley@umail.ucsb.edu>
2019-01-31 14:22:10 -0800

    new merging branch

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask,request
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config.from_object(Config)
-
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+from models import Book
 from app import routes
--- config.py
+++ config.py
@@ -1,4 +1,29 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-the_secret_key!WhereIsEvery1?'
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    ##SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
