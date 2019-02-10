5ce96b93ff879234363be41457a7cafdafb62e4b
Andrew Price <agprice@umail.ucsb.edu>
2019-02-10 11:43:10 -0800

    AP Just cleaning up formatting on the files.

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    ##SECRET_KEY = 'this-really-needs-to-be-changed'
+    # #SECRET_KEY = 'this-really-needs-to-be-changed'
     # Uncomment this to enable database connection.
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
--- models.py
+++ models.py
@@ -7,12 +7,13 @@ class Building(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    rooms = db.relationship('Room', backref='ownerBuilding', lazy=True)
+
     def __repr__(self):
         return "<Building: {}>".format(self.name)
 
     def __init__(self, name):
         self.name = name
-
 
     def serialize(self):
         return {
@@ -30,16 +31,16 @@ class Room(db.Model):
     ranges = db.Column(db.String)
     room_type = db.Column(db.String)
 
-    building_id = db.Column(db.Integer, db.ForeignKey("building.id"))
-    building = db.relationship("Building", backref=db.backref(
-        "room", order_by=id), lazy=True)
+    building_id = db.Column(db.Integer, db.ForeignKey("building.id"), nullable=False)
+    
+#    building = db.relationship("Building", backref=db.backref(
+#        "room", order_by=id), lazy=True)
 
     def __init__(self, name, roomnumber, ranges, room_type):
         self.name = name
-        self.roomnumber= roomnumber
-        self.ranges= ranges
+        self.roomnumber = roomnumber
+        self.ranges = ranges
         self.room_type = room_type
-
 
     def serialize(self):
         return {
