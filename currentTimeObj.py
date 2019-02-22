1c37595dbbf8324d50ba4db869e4e2d45118ea25
yulunchan <43765385+yulunchan@users.noreply.github.com>
2019-02-22 02:14:55 -0800

    Add files via upload
    123

--- currentTimeObj.py
+++ currentTimeObj.py
@@ -4,7 +4,39 @@ import pytz
 
 date_format='%H%M'
 date = datetime.now(tz=pytz.utc)
-date = date.astimezone(timezone('US/Pacific'))
+date = date.astimezone(timezone('US/Pacific')) # get TimeZone for SB
 intTime = int(date.strftime(date_format))
-print (intTime)
+print ("current time as int: ", intTime)
 ################## int Time obj for current time###########################
+
+
+####break down intTime for comparison purpose######
+hours = intTime // 100
+minutes = intTime % 100
+TotalTimeInMinutes = (hours * 60) + minutes
+print("hours:",hours)
+print("minutes:",minutes)
+print("sum of time in minutes:",TotalTimeInMinutes, " minutes")
+
+#####example for available time in mins######
+# let time A = current Time
+# let time S = StartTime(look at func.py  the while loop for if available)
+# let time E = Endtime(look at func.py)
+# let time A >= S and A < E
+
+timeA = 150   # 0150 or 1:50am
+timeE = 520   # 0520 or 5:20am
+
+# let say i search for room at time 1:50am
+# and the room is available from 1:00am to 5:20am
+# i can obtain the remaining available time of the room doinf this........
+TotalTimeA = ((timeA // 100) * 60) + (timeA % 100)
+TotalTimeE = ((timeE // 100) * 60) + (timeE % 100)
+availableTimeInMins = TotalTimeE - TotalTimeA
+
+print("you have", availableTimeInMins, "minutes available for this room")
+
+remainHours = availableTimeInMins // 100
+remainMinutes = availableTimeInMins % 100
+
+print("you have",remainHours,"hour/s",remainMinutes, "minutes left for this room")
--- func.py
+++ func.py
@@ -1,18 +1,34 @@
 #parse and turn into int obj
 #store
+#simple ideas on how to turn data from result.html into useful form
+from datetime import datetime
+from pytz import timezone
+import pytz
 import re
 import copy
 
+
 #set this =  data from result.html (the time ranges) and move it inside the parseAndStore function
+#following is a sample result (str0)
 str0 =": :4:00pm-4:50pm::11:00am-12:15pm::5:00pm-5:50pm::1:00pm-1:50pm::2:00pm-3:15pm:"
+
+###current time####
+date_format='%H%M'
+date = datetime.now(tz=pytz.utc)
+date = date.astimezone(timezone('US/Pacific'))
+intTime = int(date.strftime(date_format))
+print (intTime)
+
+#########################
 
 def parse(str0):
     str1 = str0.replace("-","::")
     str1 = str1.replace(" ","")
     time = str1.split("::")
 
-    objList = [];
-    available = [];
+    objList = [];   #list hold all unavailable times
+    available = [];  #list hold pair of available times  ex: available from 700-1100 (in strings)
+    availableList = []; #list hold all available time from 700-2359 (as integers)
 
     for range in time:
         range = range.replace(":","")
@@ -35,6 +51,7 @@ def parse(str0):
     objList.sort()
     print(objList)      # now objList has all the unavailable time
 # everything is store in a list at an int
+# hardcoded 7am to 1159pm for available time ranges
 
     availableStart = objList[1::2]
     availableStart.append(700)
@@ -42,19 +59,44 @@ def parse(str0):
     availableEnd.append(2359)
     availableStart.sort()
     availableEnd.sort()
-    print(availableStart)
-    print(availableEnd)
+    print(availableStart)   # list with available start time
+    print(availableEnd)    # list with available end time
 
     i = 0
     while i < len(availableStart):
         startTime = availableStart[i]
         startTime =str(startTime)
+        intStartTime = int(startTime)
+        availableList.append(intStartTime)
+
         endTime = availableEnd[i]
         endTime = str(endTime)
+        intEndTime = int(endTime)
+        availableList.append(intEndTime)
+
         output = "Available from: " + startTime + " to " + endTime
         available.append(output)
         i += 1
 
     print(available)
+    print("........................")
+    print(availableList)
+
+    #######if available implementation########
+    #availableList is a combination of startTime list and Endtime objList
+    # room is available if current time is in between start and end time
+
+    j = 0
+    TimeOfAvailability = 0
+    ifavailable = "not available at this moment"
+    while j < len(availableList):
+        if(intTime >= availableList[j] and intTime < availableList[j+1]):
+            ifavailable = "available at this moment"
+            break
+            print(ifavailable)
+        j += 2
+
+    print(ifavailable)
+
 
 parse(str0)
