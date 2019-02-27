d8cda2d40042361802f7669b250431ea5c810b0a
yulunchan <43765385+yulunchan@users.noreply.github.com>
2019-02-27 11:54:00 -0800

    Add files via upload
    ci

---
+++ test_db.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+import pytest
+from __init__ import db
+from models import Building, Room, Day
+
+
+def test_database0():
+
+	assert Building.__tablename__ == "building"
+
+def test_database1():
+
+	assert Room.__tablename__ == "room"
+
+def test_database2():
+
+	assert Day.__tablename__ == "day"
+
+def test_time_in_minutes():
+    rm = Room()
+
+    assert rm.time_in_minutes("12:03PM") == 723
