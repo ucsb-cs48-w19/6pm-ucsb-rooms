51998fbc45885dd75cada2d9ef6759715884d060
yulunchan <43765385+yulunchan@users.noreply.github.com>
2019-02-26 20:00:07 -0800

    for testing pytest

--- currentTimeObj.py
+++ currentTimeObj.py
@@ -14,9 +14,9 @@ print ("current time as int: ", intTime)
 hours = intTime // 100
 minutes = intTime % 100
 TotalTimeInMinutes = (hours * 60) + minutes
-print("hours:",hours)
-print("minutes:",minutes)
-print("sum of time in minutes:",TotalTimeInMinutes, " minutes")
+#print("hours:",hours)
+#print("minutes:",minutes)
+#print("sum of time in minutes:",TotalTimeInMinutes, " minutes")
 
 #####example for available time in mins######
 # let time A = current Time
@@ -24,19 +24,24 @@ print("sum of time in minutes:",TotalTimeInMinutes, " minutes")
 # let time E = Endtime(look at func.py)
 # let time A >= S and A < E
 
-timeA = 150   # 0150 or 1:50am
-timeE = 520   # 0520 or 5:20am
 
 # let say i search for room at time 1:50am
 # and the room is available from 1:00am to 5:20am
 # i can obtain the remaining available time of the room doinf this........
-TotalTimeA = ((timeA // 100) * 60) + (timeA % 100)
-TotalTimeE = ((timeE // 100) * 60) + (timeE % 100)
-availableTimeInMins = TotalTimeE - TotalTimeA
+def getAvailableTime(timeA, timeE):
+    TotalTimeA = ((timeA // 100) * 60) + (timeA % 100)
+    TotalTimeE = ((timeE // 100) * 60) + (timeE % 100)
+    availableTimeInMins = TotalTimeE - TotalTimeA
 
-print("you have", availableTimeInMins, "minutes available for this room")
+    #print("you have", availableTimeInMins, "minutes available for this room")
 
-remainHours = availableTimeInMins // 60
-remainMinutes = availableTimeInMins % 60
+    remainHours = availableTimeInMins // 60
+    remainMinutes = availableTimeInMins % 60
+    ##
 
-print("you have",remainHours,"hour/s",remainMinutes, "minutes left for this room")
+    #print("you have",remainHours,"hour/s",remainMinutes, "minutes left for this room")
+    return getAvailableTime
+
+#timeA = 150   # 0150 or 1:50am
+#timeE = 520   # 0520 or 5:20am
+#pass in time ranges
