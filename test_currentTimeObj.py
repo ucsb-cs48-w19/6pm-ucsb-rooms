b5c51e28e7386129cde0182f64043c85e9581311
yulunchan <43765385+yulunchan@users.noreply.github.com>
2019-02-26 21:21:01 -0800

    Update test_currentTimeObj.py

--- test_currentTimeObj.py
+++ test_currentTimeObj.py
@@ -1,17 +1,17 @@
 import pytest
 from currentTimeObj import getAvailableTime
 
-def getAvailableTime_0():
+def test_getAvailableTime_0():
   assert getAvailableTime(150, 520) == pytest.approx(210)
 
-def getAvailableTime_1():
+def test_getAvailableTime_1():
   assert getAvailableTime(250, 520) == pytest.approx(150)
 
-def getAvailableTime_2():
+def test_getAvailableTime_2():
   assert getAvailableTime(350, 520) == pytest.approx(90)
 
-def getAvailableTime_3():
+def test_getAvailableTime_3():
   assert getAvailableTime(450, 520) == pytest.approx(30)
   
-def getAvailableTime_4():
+def test_getAvailableTime_4():
   assert getAvailableTime(519, 520) == pytest.approx(1)
