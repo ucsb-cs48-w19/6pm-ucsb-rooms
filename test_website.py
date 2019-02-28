4d6362f9e48a1490efeff0a198568c3d57cf005e
zholoien <zholoien@ucsb.edu>
2019-02-27 16:07:03 -0800

    Create test_website.py

---
+++ test_website.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+import re
+import sys
+import pytest
+
+def test_website():
+        options = Options()
+        options.add_argument("--headless")  #Commented out for testing purposes
+        #self.driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver'),   options=self.options)
+        platform = sys.platform
+
+        if (platform == "linux"):
+            driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver_linux'),   options=options)
+        else:
+            driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver_mac'),   options=options)
+        driver.get("https://ucsb-rooms.herokuapp.com/")
+
+        assert None != driver.find_element_by_xpath("//input[@value='Submit']")
+
+
+        driver.close()
+
+
