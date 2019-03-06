0cd25787e7403f6291afa3d133303fcc115007dd
Andrew Price <agprice@umail.ucsb.edu>
2019-03-06 12:14:49 -0800

    AP Fixed a small error in switching room days.

--- app/routes.py
+++ app/routes.py
@@ -7,14 +7,8 @@ from app.forms import LoginForm
 from app.time_formatter import get_day_pst
 from app.time_formatter import get_time_pst
 
-# from datetime import date
-# import calendar
-
-
 @app.route("/")
 def home():
-    #form = LoginForm()
-    #return render_template('home.html', form=form)
     buildings = Building.query.all()
 
     return render_template('home.html', placeholder="Building", room_placeholder="", buildings=buildings)
@@ -24,21 +18,16 @@ def home():
 def result():
     if request.method == 'GET':
         result = request.args.to_dict()
-        result['Building'] = request.args.get('Building').upper();
-        result['Room'] = request.args.get('Room');
-        #print("THIS IS THE DIR========",dir(result))
+        result['Building'] = request.args.get('Building').upper()
+        result['Room'] = request.args.get('Room').upper()
         if (result.get("Day") == "TODAY"):
             result["Day"] = get_day_pst()
-        #print("RESULTS ARE A: ", help(result))
         name = result.get("Building")
-        rn = request.args.get('Room')
+        rn = result.get('Room')
         allBuildings = ""
         try:
-            # print(Room.query.first())
-            # print(Building.query.first().rooms)
 
              building = Building.query.filter_by(name=name).first()
-#              print("We got the building:",name,". It look like:",building)
              rooms = []
              if (building != None):
                 rooms = building.rooms
@@ -46,13 +35,13 @@ def result():
                     r.free_time(result.get("Day"), get_time_pst())
 
                 rooms.sort()
-#                 print("YOUR ROOMS SORTED LOOK LIKE",rooms)
              else:
                  buildings = Building.query.all()
                  return render_template("home.html", placeholder='Invalid Building', buildings=buildings)
 
              if(rn != ""):
                  id=building.id
+                 
                  room = Room.query.filter(Room.building_id==id, Room.roomnumber==rn).first()
                  if(room!=None):
                      time = get_time_pst()
@@ -80,15 +69,16 @@ def room():
     time = get_time_pst()
     if (result.get("Day") == "TODAY"):
         result["Day"] = get_day_pst()
-    id = int(request.args.get('BuildingID'))
-    #building = Building.query.filter_by(name=name).first()
+    name = request.args.get('Building')
 
+    building = Building.query.filter_by(name=name).first()
+    id=building.id
+     
     rn = request.args.get('Room')
-
     room = Room.query.filter(Room.building_id==id, Room.roomnumber==rn).first()
+
     room.free_time(result.get("Day"),get_time_pst())
-#    print(room.time_list)
-#     print(room.free_time(result.get("Day"),get_time_pst()))
+
     return render_template("room.html", result=result, room=room, time=time)
 
 
